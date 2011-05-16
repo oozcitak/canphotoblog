fd5c39c01fa0b0a338f185d538da4f6bae3d6d05
Ozgur Ozcitak <oozcitak@gmail.com>
2011-05-16 22:13:14 +0000

    Refactored script.

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,10 @@
 // Live preview of background image
-$('#bgcolor').change(function() {
-  applyBackground();
-});
-
-$('#bgimage').change(function() {
-  applyBackground();
-});
-
-function applyBackground() {
+$('#bgcolor, #bgimage').change(function() {
   bgcolor = $('#bgcolor').val();
   bgimage = $('#bgimage').val();
   $('body').css('background-color', bgcolor);
   $('body').css('background-image', 'url("../img/backgrounds/' + bgimage + '")');
-}
+});
 
 // Hide flash boxes on timeout
 setTimeout(function() {
